9f12e48df4f5fe41cc3398f5e9af304789f8b828
Vinicius Andrasy <vinicius.andrasy@hotmail.com>
2021-11-11 17:05:03 -0300

    First commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float fowardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce (0, 0, fowardForce * Time.deltaTime);
+		if(Input.GetKey("d")){
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if(Input.GetKey("a")){
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (rb.position.y < 0f) {
+			FindObjectOfType<GameManager> ().EndGame ();
+		}
+			
+	}
+}
